3b04cb2230f4ffa29865a3b73e0edd32e56bf9ae
Ryan <ryanhartz9169@gmail.com>
2025-01-22 15:50:03 -0600

    January 22nd Class
    Updated the ball movement script and started on the part of the script handling a launching mechanic. Only variables implemented for now!

--- Assets/scripts/BallBehavior.cs
+++ Assets/scripts/BallBehavior.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BallBehavior : MonoBehaviour
 {
 
+    //base movement variables
     public float minX = -9.78f;
     public float maxX = 9.8f;
     public float minY = -4.2f;
@@ -10,45 +11,73 @@ public class BallBehavior : MonoBehaviour
     public float minSpeed;
     public float maxSpeed;
 
-    Vector2 targetPosistion;
+    Vector2 targetPosition;
     public int secondsToMaxSpeed;
 
+    //launch variables
+    public gameObject target;
+    public float minLaunchSpeed;
+    public float maxLaunchSpeed;
+    public float minTimeToLaunch;
+    public float maxTimeToLaunch;
+    public float cooldown;
+    public bool launching;
+    public float launchDuration;
+    public float timeLastLaunch;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        secondsToMaxSpeed = 30;
-        minSpeed = 0.75f;
-        maxSpeed = 2.0f;
-        targetPosistion = getRandomPosistion();
+        //secondsToMaxSpeed = 30;
+        //minSpeed = 0.001f;
+        //maxSpeed = 0.75f;
+        targetPosition = getRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //go to object attached to script, get the transform component, look at it's posistion
-        Vector2 currentPosistion = gameObject.GetComponent<Transform>().posistion;    
+        //go to object attached to script, get the transform component, look at it's position
+        Vector2 currentPosition = gameObject.GetComponent<Transform>().position;    
+        float distance = Vector2.Distance(targetPosition, currentPosition);
 
-        if(targetPosistion != currentPosistion)
+        if(distance > 0.1)
         {
-            float currentSpeed = minSpeed;
-            Vector2 newPosistion = Vector2.MoveTowards(currentPosistion, targetPosistion, currentSpeed);
-            transform.posistion = newPosistion;
+            float currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, getDifficultyPercentage());
+            //This line says how much time has passed since the last check, so that framerate doesn't make things run faster. deltaTime might be 0.2 with an fps of 50 and 0.1 with an fps of 100, so it's equalized.
+            currentSpeed = currentSpeed * Time.deltaTime;
+            Vector2 newPosition = Vector2.MoveTowards(currentPosition, targetPosition, currentSpeed);
+            transform.position = newPosition;
         }
 
         else
         {
-            targetPosistion = getRandomPosistion();
+            targetPosition = getRandomPosition();
         }
 
-        getRandomPosistion();
+        getRandomPosition();
     }
 
-    Vector2 getRandomPosistion()
+    Vector2 getRandomPosition()
     {
         float randomX = Random.Range(minX, maxX);
         float randomY = Random.Range(minY, maxY);
         
         Vector2 v = new Vector2(randomX, randomY);
         return v;
+    }
+
+    private float getDifficultyPercentage()
+    {
+        //clamp01 is normalizing the number, making the number between the range of 0 and 1
+        float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad / secondsToMaxSpeed);
+
+        return difficulty;
+    }
+
+    private void launch()
+    {
+        return;
     }
 }
