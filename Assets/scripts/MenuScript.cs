ade822cb168e673d1b20f220586e6c6be51f48b5
Ryan <ryanhartz9169@gmail.com>
2025-02-21 15:50:28 -0600

    Added music
    Made menus properly functional and also added menu and button music! No collision sounds yet though

--- Assets/scripts/MenuScript.cs
+++ Assets/scripts/MenuScript.cs
@@ -1,9 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class MenuScript : MonoBehaviour
 {
     public void gotoGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+        StartCoroutine(WaitForSoundAndTransistion("MainGame"));
+    }
+
+    public void gotoMenu()
+    {
+        StartCoroutine(WaitForSoundAndTransistion("MainMenu"));
+    }
+
+    public void gotoPin()
+    {
+        StartCoroutine(WaitForSoundAndTransistion("CharecterSelection"));
+    }
+
+    private IEnumerator WaitForSoundAndTransistion(string sceneName)
+    {
+        AudioSource audio = GetComponentInChildren<AudioSource>();
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
--- Assets/scripts/SelectionScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SelectionScript : MonoBehaviour
-{
-    public void gotoMenu()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-    }
-}
